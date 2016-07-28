3d0097d2e3e615833e57faebec0355399b4ecbb1
AraHaan <seandhunt_7@yahoo.com>
2016-07-28 13:59:33 -0500

    Changed Game and added some vars to size some things down.

--- Cheesesynth.js
+++ Cheesesynth.js
@@ -1,7 +1,7 @@
 const Eris = require("eris");
 
 var botVersion = "v0.0.1.1 alpha";
-var gameName = "Cheese.lab";
+var gameName = "Cheese.lab Industries Inc.";
 var prefix = "."
 
 var bot = new Eris("  "); //REMOVED TOKEN
@@ -37,10 +37,12 @@ bot.on("messageCreate", (msg) => {
     { bot.createMessage(msg.channel.id, "( ͡° ͜ʖ ͡°)");}
     else if (cmd === "info")
     {
-      bot.createMessage(msg.channel.id,
+      var msgchan = msg.channel.id;
+      var usrid = msg.author.id;
+      bot.createMessage(msgchan,
         "__**"+msg.member.user.username.toUpperCase() + "'S OFFICIAL ID CARD - ACCESS CODE: #"+msg.member.user.discriminator+"**__" + "\n"
       + "```ruby\n"
-      +"\n"+"           ID: "+msg.author.id
+      +"\n"+"           ID: "+usrid
       +"\n"+"         Name: "+msg.member.user.username
       +"\n"+"     Nickname: "+msg.member.nick
       +"\n"+"Discriminator: "+msg.member.user.discriminator
@@ -49,7 +51,7 @@ bot.on("messageCreate", (msg) => {
       +"\n"+"       Joined: "+msg.channel.guild.name+" on "+msg.member.joinedAt
       +"\n"+"       Avatar: "
       + "```"
-      +"\n"+ "https://cdn.discordapp.com/avatars/"+msg.author.id+"/"+msg.author.avatar+".jpg"
+      +"\n"+ "https://cdn.discordapp.com/avatars/"+usrid+"/"+msg.author.avatar+".jpg"
     );
     }
     else if (cmd === "test")
