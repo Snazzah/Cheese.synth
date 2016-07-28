77c064676f1ee13f9420cb6ff1091f75fd642bb5
Cheese <stuffalltech@live.co.uk>
2016-07-28 19:33:15 +0100

    Cleaned up the code
    Added a cmd variable

--- Cheesesynth.js
+++ Cheesesynth.js
@@ -4,7 +4,7 @@ var botVersion = "v0.0.1 alpha";
 var gameName = "Cheese.lab";
 var prefix = "."
 
-var bot = new Eris(" "); //REMOVED TOKEN//
+var bot = new Eris("  "); //REMOVED TOKEN
 bot.on("ready", () => {
     console.log("Cheese.synth is unloading Cheese.puns! Please wait...");
     console.log("Finished loading Cheese.puns. Unpacking Cheese.food...");
@@ -17,26 +17,26 @@ bot.on("ready", () => {
 });
 
 bot.on("messageCreate", (msg) => {
-      var msgGame;
-      try {
-          msgGame = "Playing "+msg.member.game.name;
-      } catch(err) {
-          msgGame = "Playing "+"None";
-      }
-    if(msg.content.toLowerCase()  === prefix + "ping") {
-        bot.createMessage(msg.channel.id, "Pong!");
-        console.log("<"+"@"+msg.author.id+">" + " pinged the server!");
-    } else if (msg.content.toLowerCase()  === prefix +"commands") {
-      bot.createMessage(msg.channel.id, msg.author.mention+" "+"Coming SOON:tm:");
-    }else if(msg.content.toLowerCase()  === prefix +"pong") {
-        bot.createMessage(msg.channel.id, "Ping!");
-    }else if(msg.content.toLowerCase()  === prefix +"cheese") {
-      bot.createMessage(msg.channel.id, ":cheese:");
-    }
-    else if (msg.content.toLowerCase()  === prefix +"lenny") {
-      bot.createMessage(msg.channel.id, "( ͡° ͜ʖ ͡°)");
-    }
-    else if (msg.content.toLowerCase()  === prefix +"info") {
+        var msgGame;
+        try {msgGame = "Playing "+msg.member.game.name;}
+        catch(err) { msgGame = "Playing "+"None";}
+
+    if(msg.content.startsWith(prefix))
+    {
+    var cmd = msg.content.substring( prefix.length ).toLowerCase();
+    if(cmd  === "ping")
+    { bot.createMessage(msg.channel.id, "Pong!");
+    console.log("<"+"@"+msg.author.id+">" + " pinged the server!");}
+    else if (cmd === "commands")
+    { bot.createMessage(msg.channel.id, msg.author.mention+" "+"Coming SOON:tm:");}
+    else if(cmd === "pong")
+    { bot.createMessage(msg.channel.id, "Ping!");}
+    else if(cmd === "cheese")
+    { bot.createMessage(msg.channel.id, ":cheese:");}
+    else if (cmd === "lenny")
+    { bot.createMessage(msg.channel.id, "( ͡° ͜ʖ ͡°)");}
+    else if (cmd === "info")
+    {
       var codeBlock = "```";
       bot.createMessage(msg.channel.id,
         "__**"+msg.member.user.username.toUpperCase() + "'S OFFICIAL ID CARD - ACCESS CODE: #"+msg.member.user.discriminator+"**__" + "\n"
@@ -53,23 +53,20 @@ bot.on("messageCreate", (msg) => {
       +"\n"+ "https://cdn.discordapp.com/avatars/"+msg.author.id+"/"+msg.author.avatar+".jpg"
     );
     }
-    else if (msg.content.toLowerCase() === prefix + "test") {
-    bot.createMessage(msg.channel.id, msg1);
-    }
-    else if (msg.content.toLowerCase() === "hi!" || msg.content.toLowerCase() === "hello!") {
-      bot.createMessage(msg.channel.id, "<@"+msg.author.id+"> Welcome to the **"+msg.channel.guild.name+"** Server!");
-      bot.createMessage(msg.channel.id, "*flips table*");
-      bot.createMessage(msg.channel.id, "(╯°□°）╯︵ ┻━┻");
-    }
-    else if (msg.content.toLowerCase()  === prefix +"setgame") {
-      null;
-    }
-    else if (msg.content.toLowerCase()  === prefix +"version") {
-      bot.createMessage(msg.channel.id, "**Bot Version: **" + botVersion);
-    }
-    else if (msg.content.toLowerCase()  === prefix +"game") {
-      bot.createMessage(msg.channel.id, "My current game is set to **" + gameName + "** by the owner! Only the owner can set this.");
-    }
+    else if (cmd === "test")
+    {bot.createMessage(msg.channel.id, msg1);}
+    else if (cmd === "hi!" || cmd === "hello!")
+    {bot.createMessage(msg.channel.id, "<@"+msg.author.id+"> Welcome to the **"+msg.channel.guild.name+"** Server!");
+    bot.createMessage(msg.channel.id, "*flips table*");
+    bot.createMessage(msg.channel.id, "(╯°□°）╯︵ ┻━┻");}
+    else if (cmd === "setgame")
+    {null;}
+    else if (cmd === "version")
+    {bot.createMessage(msg.channel.id, "**Bot Version: **" + botVersion);}
+    else if (cmd === "game")
+    {bot.createMessage(msg.channel.id, "My current game is set to **" + gameName + "** by the owner! Only the owner can set this.");}
+
+  }
 });
 
 bot.connect();
